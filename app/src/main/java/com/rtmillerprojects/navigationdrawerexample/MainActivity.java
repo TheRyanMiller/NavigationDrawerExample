9801cb62554fb6e5cfb93531fe532e55c62039e3
Ryan <rmiller07@gmail.com>
2016-05-28 14:55:25 -0400

    functional BaseActivity class contains navigation drawer

--- app/src/main/java/com/rtmillerprojects/navigationdrawerexample/MainActivity.java
+++ app/src/main/java/com/rtmillerprojects/navigationdrawerexample/MainActivity.java
@@ -1,38 +1,21 @@
 package com.rtmillerprojects.navigationdrawerexample;
 
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    private DrawerLayout drawerLayout;
-    private ListView listView;
-    private String[] planets;
-    private Toolbar toolbar;
-    @Override
+public class MainActivity extends BaseActivity {
+
+    DrawerLayout drawerLayout;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        planets = getResources().getStringArray(R.array.planets);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        listView = (ListView) findViewById(R.id.navList);
-        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,planets));
-        listView.setOnItemClickListener(this);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("WAZZUP");
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, planets[position], Toast.LENGTH_LONG).show();
-    }
+
 }
