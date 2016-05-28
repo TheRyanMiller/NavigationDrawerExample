848f53d519411b58ef1eea68a3beed25a37e1ee9
Ryan <rmiller07@gmail.com>
2016-05-27 23:21:35 -0400

    updates

--- app/src/main/java/com/rtmillerprojects/navigationdrawerexample/MainActivity.java
+++ app/src/main/java/com/rtmillerprojects/navigationdrawerexample/MainActivity.java
@@ -3,15 +3,19 @@ package com.rtmillerprojects.navigationdrawerexample;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private DrawerLayout drawerLayout;
     private ListView listView;
     private String[] planets;
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         listView = (ListView) findViewById(R.id.navList);
         listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,planets));
+        listView.setOnItemClickListener(this);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("WAZZUP");
+    }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, planets[position], Toast.LENGTH_LONG).show();
     }
 }
